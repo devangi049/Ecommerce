dc0d1d2489c70320cb12a525c69b2f77a7c39525
Devangi2006 <pateldevu610@gmail.com>
2025-01-13 23:05:08 +0530

    Ecomm

--- Ecomm/Ecomm/settings.py
+++ Ecomm/Ecomm/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,8 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True  
 ROOT_URLCONF = 'Ecomm.urls'
 
 TEMPLATES = [
